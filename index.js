ab4e59bd8f971957dc2f486e1357978ab2c48e86
Gregor Tudan <gregor.tudan@cofinpro.de>
2019-10-15 19:17:17 +0200

    added test

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
-(async () => {
-
-    const bpmnXML = fs.promises.readFile('diagram.bpmn', 'utf-8');
-
+async function renderDiagram(bpmnXML, options) {
     let browser;
     try {
         browser = await puppeteer.launch({
@@ -18,11 +15,11 @@ const puppeteer = require('puppeteer');
                 deviceScaleFactor: 2
             }
         });
+
         const page = await browser.newPage();
         page.on('console', msg => console.log("BPMN-js: " + msg.text()));
 
         await page.goto(`file://${path.join(__dirname, 'index.html')}`);
-
         const svg = await page.$eval('#container', (container, bpmnXML) => {
             const viewer = new BpmnJS({container: '#container'});
             return new Promise((resolve, reject) => {
@@ -45,23 +42,31 @@ const puppeteer = require('puppeteer');
                     }
                 });
             });
-        }, await bpmnXML);
+        }, bpmnXML);
 
-       await Promise.all([
+        await Promise.all([
             fs.promises.writeFile('diagram.svg', svg),
             page.screenshot({omitBackground: true, type: 'png'})
                 .then(png => fs.promises.writeFile('diagram.png', png)),
             page.screenshot({type: 'jpeg'})
-                .then(jpeg =>  fs.promises.writeFile('diagram.jpeg', jpeg)),
+                .then(jpeg => fs.promises.writeFile('diagram.jpeg', jpeg)),
             page.pdf({landscape: true, format: 'a4', pageRange: 1, preferCSSPageSize: true})
                 .then(pdf => fs.promises.writeFile('diagram.pdf', pdf))
         ]);
+
     } catch (e) {
         console.error('Failed to generate diagram', e);
     } finally {
         browser.close();
     }
-})();
+}
+
+if (require.main === module) {
+    const bpmnXML = fs.readFileSync('diagram.bpmn', {encoding: 'utf-8'});
+    renderDiagram(bpmnXML)
+        .then(() => console.log("Export complete"))
+        .catch(console.error);
+}
 
 
-console.log("Export complete");
+module.exports = renderDiagram;
---
+++ test/test.js
@@ -0,0 +1,12 @@
+const bpmnCli = require('../index');
+const fs = require('fs');
+const assert = require('assert');
+
+describe('BPMN-js.cli', function () {
+    it('should render a svg', async function() {
+        const bpmnXML = fs.readFileSync('diagram.bpmn', {encoding: 'utf-8'});
+        await bpmnCli(bpmnXML);
+
+        assert(fs.existsSync('diagram.svg'))
+    });
+});
