a361c6f18a315dc601a5b36d5d7fe8f6d8cb7938
Gregor Tudan <gregor.tudan@cofinpro.de>
2019-10-15 21:39:46 +0200

    try without special image

--- test/test.js
+++ test/test.js
@@ -2,11 +2,8 @@ const bpmnCli = require('../index');
 const fs = require('fs');
 const assert = require('assert');
 
-
-
-
 describe('BPMN-js.cli', function () {
-    this.timeout(5000);
+    this.timeout(30000);
 
     it('should render a svg', async function() {
         const bpmnXML = fs.readFileSync('diagram.bpmn', {encoding: 'utf-8'});
