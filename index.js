7f2fa1ffe8218dd5de76e90ad259f84166a0c4f1
Gregor Tudan <gregor.tudan@cofinpro.de>
2019-10-15 15:02:26 +0200

    create png screenshot

--- index.js
+++ index.js
@@ -5,30 +5,42 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const bpmnXML = fs.readFileSync('sample.bpmn', 'utf-8');
+    const bpmnXML = fs.readFileSync('diagram.bpmn', 'utf-8');
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({defaultViewport: {width: 1440, height: 900, landscape: true, deviceScaleFactor: 2}});
     const page = await browser.newPage();
-    page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+    page.on('console', msg => console.log("BPMN-js: " + msg.text()));
 
     await page.goto(`file://${path.join(__dirname, 'index.html')}`);
     const svg = await page.$eval('#container', (container, bpmnXML) => {
+        const viewer = new BpmnJS({container: '#container'});
         return new Promise((resolve, reject) => {
-            const viewer = new BpmnJS({container: '#container'});
             viewer.importXML(bpmnXML, function (err) {
                 if (err) {
                     console.log('error rendering', err);
-                    reject();
+                    reject(err);
                 } else {
-                    console.log('we are good!');
+                    console.log('Diagram looks valid');
+                    const canvas = viewer.get('canvas');
+                    canvas.zoom('fit-viewport');
                     viewer.saveSVG((err, svg) => {
-                        resolve(svg);
-                    })
+                        if (err) {
+                            console.log('Failed to export', err);
+                            reject(err);
+                        } else {
+                            resolve(svg);
+                        }
+                    });
                 }
             });
         });
     }, bpmnXML);
 
-    fs.writeFileSync('diagram.svg', svg)
+    const screenshot = await page.screenshot({omitBackground: true, type: 'png'});
 
+    browser.close();
+
+    fs.writeFileSync('diagram.png', screenshot);
+    fs.writeFileSync('diagram.svg', svg);
+    console.log("Export complete");
 })();
